8fe8b4ee2c0168c898ecb2dadf1b34f51f47c438
excusasclub <elclubdelaexcusa@gmail.com>
2025-09-17 11:36:37 +0200

    Vista Panel lateral solo diseño

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('baglist/<int:baglist_id>/', views.baglist_detail, name='baglist_detail'),
+    path('baglists/', views.baglist_edition, name='baglist_edition'),
+    path('baglists/<int:baglist_id>/', views.baglist_detail, name='baglist_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,9 +3,23 @@ from .models import BagList
 
 def baglist_detail(request, baglist_id):
     baglist = get_object_or_404(BagList, id=baglist_id)
-    tbaglists = baglist.tbaglists.all()  # Trae todas las sublistas
+    tbaglists = baglist.tbaglists.all()  # Sublistas de la baglist actual
+
+    # Todas las BagLists del usuario para la sección "Mis BagLists"
+    baglists = BagList.objects.filter(user=request.user)
+
     context = {
         'baglist': baglist,
         'tbaglists': tbaglists,
+        'baglists': baglists,  # <-- Aquí
     }
     return render(request, 'core/baglist_detail.html', context)
+
+def baglist_edition(request):
+    # Todas las BagLists del usuario
+    baglists = BagList.objects.filter(user=request.user)
+    context = {
+        'baglists': baglists,
+    }
+    return render(request, 'core/baglist_edition.html', context)
+
